16f809d10eb1b61c63dd1c99cf8896b24aa2a163
Daniella Marks <marks.daniella@gmail.com>
2021-05-12 16:13:41 -0700

    first test passed for locations

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,116 +1,56 @@
-import app from '../lib/app.js';
-import supertest from 'supertest';
-import client from '../lib/client.js';
-import { execSync } from 'child_process';
+// import app from '../lib/app.js';
+// import supertest from 'supertest';
+// import client from '../lib/client.js';
+// import { execSync } from 'child_process';
+import { formatLocations } from '../lib/utils';
+import location from '../data/location.js';
 
-const request = supertest(app);
 
-describe('API Routes', () => {
+describe('Data Munging', () => {
 
-  beforeAll(() => {
-    execSync('npm run setup-db');
-  });
+  const expectedLocations = [
+    {
 
-  afterAll(async () => {
-    return client.end();
-  });
-
-  const expectedCats = [
-    {
-      id: expect.any(Number),
-      name: 'Felix',
-      type: 'Tuxedo',
-      url: 'cats/felix.png',
-      year: 1892,
-      lives: 3,
-      isSidekick: false
+      'formatted_query': 'Avenida Calle 30, Barranquillita, Barranquilla, Atlántico, Caribe, 080004, Colombia',
+      'latitude': '10.9809716',
+      'longitude': '-74.7752992'
     },
     {
-      id: expect.any(Number),
-      name: 'Garfield',
-      type: 'Orange Tabby',
-      url: 'cats/garfield.jpeg',
-      year: 1978,
-      lives: 7,
-      isSidekick: false
+      'formatted_query': 'Avenida Calle 30, Barranquilla, Atlántico, Caribe, 083001, Colombia',
+      'latitude': '10.9355691',
+      'longitude': '-74.7812725'
     },
     {
-      id: expect.any(Number),
-      name: 'Duchess',
-      type: 'Angora',
-      url: 'cats/duchess.jpeg',
-      year: 1970,
-      lives: 9,
-      isSidekick: false
-    },
-    {
-      id: expect.any(Number),
-      name: 'Stimpy',
-      type: 'Manx',
-      url: 'cats/stimpy.jpeg',
-      year: 1990,
-      lives: 1,
-      isSidekick: true
-    },
-    {
-      id: expect.any(Number),
-      name: 'Sylvester',
-      type: 'Tuxedo',
-      url: 'cats/sylvester.jpeg',
-      year: 1945,
-      lives: 1,
-      isSidekick: true
-    },
-    {
-      id: expect.any(Number),
-      name: 'Tigger',
-      type: 'Orange Tabby',
-      url: 'cats/tigger.jpeg',
-      year: 1928,
-      lives: 8,
-      isSidekick: false
-    },
-    {
-      id: expect.any(Number),
-      name: 'Hello Kitty',
-      type: 'Angora',
-      url: 'cats/hello-kitty.jpeg',
-      year: 1974,
-      lives: 9,
-      isSidekick: false
-    },
-    {
-      id: expect.any(Number),
-      name: 'Hobbs',
-      type: 'Orange Tabby',
-      url: 'cats/hobbs.jpeg',
-      year: 1985,
-      lives: 6,
-      isSidekick: true
+      'formatted_query': 'Avenida Calle 30, Barranquillita, Barranquilla, Atlántico, Caribe, 080002, Colombia',
+      'latitude': '10.9860026',
+      'longitude': '-74.7763217'
     }
+  ];
+
+  const expectedWeather = [
+    {
+        "forecast": "Partly cloudy until afternoon.",
+        "time": "Mon Jan 01 2001"
+      },
+      {
+        "forecast": "Mostly cloudy in the morning.",
+        "time": "Tue Jan 02 2001"
+      },
+      {
+        "forecast": "Mostly cloudy in the morning.",
+        "time": "Tue Jan 02 2001"
+      },
   ];
 
   // If a GET request is made to /api/cats, does:
   // 1) the server respond with status of 200
   // 2) the body match the expected API data?
-  it('GET /api/cats', async () => {
-    // act - make the request
-    const response = await request.get('/api/cats');
+  it('munges location data', async () => {
+    console.log(expectedLocations);
 
-    // was response OK (200)?
-    expect(response.status).toBe(200);
-
-    // did it return the data we expected?
-    expect(response.body).toEqual(expectedCats);
-
-  });
-
-  // If a GET request is made to /api/cats/:id, does:
-  // 1) the server respond with status of 200
-  // 2) the body match the expected API data for the cat with that id?
-  test('GET /api/cats/:id', async () => {
-    const response = await request.get('/api/cats/2');
-    expect(response.status).toBe(200);
-    expect(response.body).toEqual(expectedCats[1]);
+    const output = formatLocations(location);
+    console.log(output);
+    expect(output).toEqual(expectedLocations);
   });
 });
+
